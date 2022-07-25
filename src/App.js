7f2313e258a1299e9035259e84f9ff4e72c35139
edward wang! <53409587+wang-edward@users.noreply.github.com>
2022-07-25 16:33:07 -0400

    upload react page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Speech from './Speech';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='card'>
+          <Speech/>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
