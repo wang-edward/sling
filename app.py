d4f44b5495d8c408eedf8a2827f355e078227757
xmaggieliu <xmaggie.liu@gmail.com>
2022-07-29 23:04:53 -0400

    Lang to code map

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from tkinter import *
 from python_translator import Translator
+import json
+
+
+lang_to_code = {}
+
+lang_data = json.load(open('lang.json'))
+
+for i in lang_data["text"]:
+    lang_to_code[i["language"]] = i["code"]
+
 
 root = Tk()
 
@@ -7,10 +17,12 @@ def speech():
     # set text to smt
     p['text'] = "HAHAHAHhfioewhaiofhxeiowacmjfioewhajoicfheiwoahcfioewhaiocfheiwoahcfjiehifewhaciofheiowahcuoifheiwoahxfioewhaciofhewicFHUEWAHFUOEWHAUOFBEWJAFBUOEWHAUFHESOUAIHFUOESHAFUOBEAIWUFBEIHABUARUWAH"
 
+
 root.title('SLING DEMO')
 
 canvas = Frame(root, width=800, height=500, bg="#B5D99C")
 canvas.grid(columnspan=5, rowspan=7)
+
 
 heading = Label(root, text="SLING", font=("Avenir", 32), bg="#B5D99C", fg="#333333")
 heading.grid(columnspan=3, column=1, row=0)
@@ -24,31 +36,19 @@ p.grid(columnspan=3, column=1, row=3)
 prompt = Label(root, text="Select a language in the dropdown below:", font=("Avenir", 16), bg="#B5D99C", fg="#333333")
 prompt.grid(columnspan=3, column=1, row=4)
 
+
 speech_text = StringVar()
 speech_btn = Button(root, textvariable=speech_text, command=lambda:speech(), font="Avenir", bg="#828282", fg="#FFFF82", height=2, width=10)
 speech_text.set("Speech")
 speech_btn.grid(column=3, row=5, sticky=W)
 
-options = [
-    "en",
-    "de",
-    "es",
-    "fr",
-    "zh-CN",
-    "zh-TW",
-    "ja",
-    "ko",
-    "ar",
-    "iw",
-    "hi",
-    "ur"
-]
+options = lang_to_code.keys()
 
 # Text selected in dropdown
 clicked = StringVar(value="Select:")
 
 drop = OptionMenu(root, clicked, *options)
-drop.config(font=("Avenir", 16), fg="#333333", bd=0, width=5)
+drop.config(font=("Avenir", 16), fg="#333333", bd=0, width=15)
 drop.grid(column=1, row=5, sticky=E)
 drop["bg"]="#FFFF82"
 
