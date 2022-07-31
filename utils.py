e13eea76d610cf806f6a949a1c003b7374f2e01e
wang-edward <edawordwang@gmail.com>
2022-07-30 20:48:06 -0400

    coninuously update cur_char + cur_text

--- utils.py
+++ utils.py
@@ -15,21 +15,17 @@ import re
 
 import json
 
-current_char = " "
-current_text = ""
-current_lang = "en"
+global current_char
+global current_text
+global current_lang
 
-bind_map={}
 
-def init_bindings(bmap):
-    with open('binds.json', 'r') as f:
-        bmap = json.load(f)
-        print(bmap)
 
+global bind_map
 
 def read(ser):
     values = re.sub(r"[a-z'\\]", "", str(ser.readline())).split()
-    print(values)
+    #print(values)
     return values
 
 def classify(values, bind_map):
@@ -43,6 +39,8 @@ def classify(values, bind_map):
 
     for i in range(5):
         sum += 2 ** i * conc[i]
+    ans = bind_map.get(str(sum))
+    print(ans)
     return (bind_map.get(str(sum)))
 
 def tts(read_text, read_lang):
@@ -54,14 +52,15 @@ def tts(read_text, read_lang):
     speak.save(name)
     os.system("mpg321 " + name)
 
-def update(ser, bindings, text, lang):
+def update(ser, bindings, char, text, lang):
     values = read(ser)
-    print(values[0])
+    #print(values[0])
     if (str(values[0]) == "W"):
-        current_text += current_char
+        if (current_char != ""):
+            current_text += current_char
     if (str(values[0]) == "D"):
         values.pop(0) #remove "D" from data
-        print(values)
+        #print(values)
         current_char = classify(values, bindings)
 
     if (str(values[0]) == "S"):
@@ -69,7 +68,12 @@ def update(ser, bindings, text, lang):
         audio_thread.start()
 
 def main():
-    init_bindings(bmap = bind_map)
+    current_char = "."
+    current_text = ""
+    current_lang = "en"
+    with open('binds.json', 'r') as f:
+        bind_map = json.load(f)
+    print(bind_map)
     ser = serial.Serial('/dev/cu.SLAB_USBtoUART', baudrate = 115200, timeout=1)
 
     for i in range (100):
@@ -77,7 +81,23 @@ def main():
         ser.readline()
 
     while True:
-        update(ser=ser, bindings=bind_map, text = current_text, lang = current_lang)
+        values = read(ser)
+        #print(values[0])
+        if (str(values[0]) == "W"):
+            if (current_char != None):
+                current_text += current_char
+        if (str(values[0]) == "D"):
+            values.pop(0) #remove "D" from data
+            #print(values)
+            temp = classify(values, bind_map)
+            print(temp)
+            current_char = temp
+            #current_char = classify(values, bind_map)
+
+        if (str(values[0]) == "S"):
+            audio_thread = Thread(target=tts, args=[current_text, current_lang])
+            audio_thread.start()
+        print("current_char: {0}, current_text: {1}".format(current_char, current_text))
 
 if __name__ == "__main__":
     main()
