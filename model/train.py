45a520ac6cc64bb75046cb05f3c9b14bea0c496f
Dile-the-Tile <61816265+Dile-the-Tile@users.noreply.github.com>
2022-08-06 17:19:27 -0400

    Finished model

--- model/train.py
+++ model/train.py
@@ -1,9 +1,12 @@
 # multi-class classification with Keras
 import pandas
+import json
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
@@ -25,7 +28,6 @@ dummy_y = np_utils.to_categorical(encoded_Y)
 print(X)
 print(Y)
 
-
 # define baseline model
 	# create model
 model = Sequential()
@@ -34,17 +36,29 @@ model.add(Dense(20, activation='softmax'))
 # Compile model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'], run_eagerly=True)
 
-model.fit(X,dummy_y, batch_size=1 )
+model.fit(X,dummy_y, epochs = 50, batch_size=1 )
 
-# A few random samples
-use_samples = [5, 38, 20, 13, 20]
-samples_to_predict = []
+# new instances where we do not know the answer
+dataframe2 = pandas.read_csv("random_data.csv", header=None)
+dataset2 = dataframe2.values
+xNew = dataset2[:,0:5].astype(float)
+yNew = model.predict(xNew)
 
-# Convert into Numpy array
-samples_to_predict = np.array(samples_to_predict)
+# find largest number in each instance of yNew to get the correct letter
+index_of_largest = [0] * len(xNew)
+for i in range(len(xNew)):
+	max = yNew[i][0]
+	index_of_largest[i] = 0
+	for j in range(1,len(yNew[i])):
+		if yNew[i][j] > max:
+			max = yNew[i][j]
+			index_of_largest[i] = j
 
-model.predict(samples_to_predict)
-
+with open("alphabet.json", "r") as read_file:
+	alphabet_dict = json.load(read_file)
+	# show the inputs and predicted outputs
+	for i in range(len(xNew)):
+		print("X=%s, Predicted=%s" % (xNew[i], alphabet_dict[str(index_of_largest[i])]))
 #kfold = KFold(n_splits=10, shuffle=True)
 #results = cross_val_score(estimator, X, dummy_y, cv=kfold)
 #print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
